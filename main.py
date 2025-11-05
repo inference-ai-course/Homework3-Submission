1a7e5d0ee70da459f2978a195c193321669e14e8
Linyi Jiang <jianglinyi97@gmail.com>
2025-11-05 00:14:17 -0500

    step3

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, Response, UploadFile, File
 from fastapi.responses import FileResponse
-import whisper
+from response_generation import generate_response
+from transcribe_audio import transcribe_audio
 
 app = FastAPI()
-
-asr_model = whisper.load_model("small")
-
-def transcribe_audio(audio_bytes):
-    with open("temp.wav", "wb") as f:
-        f.write(audio_bytes)
-    result = asr_model.transcribe("temp.wav")
-    return result["text"]
 
 @app.post("/chat")
 async def chat_endpoint(file: UploadFile = File(...)):
     audio_bytes = await file.read()
     user_text = transcribe_audio(audio_bytes)
     print(user_text)
+    generated_text = generate_response(user_text)
+    print(generated_text)
     # TODO: ASR → LLM → TTS
-    return FileResponse("response.wav", media_type="audio/wav")
+    return Response("")
